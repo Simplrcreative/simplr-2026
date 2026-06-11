20c4ffdbe43c82deb56dac957c4ad6dea3a1e43a
Daryl Glass- Simplr <123460448+DaryGlass@users.noreply.github.com>
2026-06-11 15:02:16 +0200

    Form color

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -197,7 +197,7 @@ export default function LandingPage() {
       {showForm && (
         <section className="px-5 pt-20">
           <div className="grid grid-cols-12 w-full">
-            <div className="col-start-8 col-span-5">
+            <div className="col-start-8 col-span-5 slide-up-subtle">
               <ContactForm style="dark" heading="This is a cool heading" />
             </div>
           </div>
